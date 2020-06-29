bd4458dbdf33f876a48d018c05c4e2a1fff337b1
AAkopyan <albert.akopyan@firstlinesoftware.com>
2020-06-29 11:32:09 +0300

    Fixed interface

--- AppRSA/MainForm.Designer.cs
+++ AppRSA/MainForm.Designer.cs
@@ -63,13 +63,22 @@
             this.buttonCreateKey = new System.Windows.Forms.Button();
             this.textBoxCreateKeyName = new System.Windows.Forms.TextBox();
             this.tabPageAboutProgram = new System.Windows.Forms.TabPage();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
             this.tabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPageDecrypt.SuspendLayout();
             this.tabPageKeys.SuspendLayout();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericCreateKeySize)).BeginInit();
+            this.tabPageAboutProgram.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl
@@ -180,7 +189,6 @@
             // 
             // tabPageDecrypt
             // 
-            this.tabPageDecrypt.Controls.Add(this.label5);
             this.tabPageDecrypt.Controls.Add(this.buttonDecrypt);
             this.tabPageDecrypt.Controls.Add(this.label11);
             this.tabPageDecrypt.Controls.Add(this.textBoxDecryptMessage);
@@ -258,7 +266,7 @@
             this.buttonDecryptChoosePblicKey.Name = "buttonDecryptChoosePblicKey";
             this.buttonDecryptChoosePblicKey.Size = new System.Drawing.Size(151, 30);
             this.buttonDecryptChoosePblicKey.TabIndex = 10;
-            this.buttonDecryptChoosePblicKey.Text = "Choose public key";
+            this.buttonDecryptChoosePblicKey.Text = "Choose private key";
             this.buttonDecryptChoosePblicKey.UseVisualStyleBackColor = true;
             this.buttonDecryptChoosePblicKey.Click += new System.EventHandler(this.buttonDecryptChoosePblicKey_Click);
             // 
@@ -438,6 +446,15 @@
             // 
             // tabPageAboutProgram
             // 
+            this.tabPageAboutProgram.Controls.Add(this.label19);
+            this.tabPageAboutProgram.Controls.Add(this.label18);
+            this.tabPageAboutProgram.Controls.Add(this.label17);
+            this.tabPageAboutProgram.Controls.Add(this.label16);
+            this.tabPageAboutProgram.Controls.Add(this.label15);
+            this.tabPageAboutProgram.Controls.Add(this.label14);
+            this.tabPageAboutProgram.Controls.Add(this.label13);
+            this.tabPageAboutProgram.Controls.Add(this.label12);
+            this.tabPageAboutProgram.Controls.Add(this.label9);
             this.tabPageAboutProgram.Location = new System.Drawing.Point(4, 29);
             this.tabPageAboutProgram.Name = "tabPageAboutProgram";
             this.tabPageAboutProgram.Padding = new System.Windows.Forms.Padding(3);
@@ -446,14 +463,86 @@
             this.tabPageAboutProgram.Text = "About program";
             this.tabPageAboutProgram.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // label9
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 246);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 17;
-            this.label5.Text = "label5";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(54, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(379, 20);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Курсовой проект по курсу \"Защита информации\"";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(19, 96);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(222, 20);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Преподаватель: Хорев В.П.";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(19, 127);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(171, 20);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "Студент: Акопян А.А.";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(19, 161);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(156, 20);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "Группа: ИД-0201-17";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(23, 229);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(430, 20);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "Тема: Программная реализация криптоалгоритма RSA";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(23, 260);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(204, 20);
+            this.label16.TabIndex = 5;
+            this.label16.Text = "Функционал приложения:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(54, 291);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(155, 20);
+            this.label17.TabIndex = 6;
+            this.label17.Text = "* Создание ключей";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(54, 311);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(277, 20);
+            this.label18.TabIndex = 7;
+            this.label18.Text = "* Шифрование по открытому ключу";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(54, 331);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(284, 20);
+            this.label19.TabIndex = 8;
+            this.label19.Text = "* Расшифровка по закрытому ключу";
             // 
             // MainForm
             // 
@@ -474,6 +563,8 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericCreateKeySize)).EndInit();
+            this.tabPageAboutProgram.ResumeLayout(false);
+            this.tabPageAboutProgram.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -515,7 +606,15 @@
         private System.Windows.Forms.Button buttonDecryptImportPublicKey;
         private System.Windows.Forms.TextBox textBoxDecryptText;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
     }
 }
 
